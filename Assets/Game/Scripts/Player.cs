82f1130fd61556efbc769ad0fba8c956524262e8
vasupatel <vasurabadiya5@gmail.com>
2023-09-22 14:04:55 +0530

    22

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -11,19 +11,25 @@ public class Player : MonoBehaviour
     {
         PLayerValue =PlayerPrefs.GetInt("Score",0);
         for(int i = 0 ; i<= PLayerValue ; i++)
-        {
+         {
             AllButons[i].interactable = true;
             AllButons[i].gameObject.transform.GetChild(1).gameObject.SetActive(false);
         }
     }
 
-    public void BtnPressed()
+    public void BtnPressed(int BtnValue)
     {
-        int Value = PlayerPrefs.GetInt("Score",0);
-        Value++;
-        PlayerPrefs. SetInt("Score", Value);
+        int Value = PlayerPrefs.GetInt("Score",0);// 7
+        if (BtnValue > Value)// 7
+        {
+            Value++;
+        }
+    
+        PlayerPrefs.SetInt("Score", Value);
+        Debug.Log("Value is = " + Value);
+        Debug.Log("Btn Value is = " + BtnValue);
 
-        if (Value > PLayerValue)
+        if (BtnValue >= Value)
         {
             for (int i = 0; i <= Value; i++)
             {
